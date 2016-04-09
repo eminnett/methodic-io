f1aff374c67ce1935b2e342930a397441044df04
Edward Minnett <ed@methodic.io>
2016-04-09 13:57:54 +0100

    Refactor: Use helpers for meta title and description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def placeholder_meta_title
+    "Methodic | Software + Research."
+  end
+
+  def placeholder_meta_description
+    'We provide Ruby and Javascript, full-stack web development and software ' \
+    'consulting in London. We also invest time exploring the disciplines of '  \
+    'data science and machine learning.'
+  end
 end
