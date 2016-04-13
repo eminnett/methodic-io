635a5efc05c5cff00fb62cc0bad7d19653bd5b29
Edward Minnett <ed@methodic.io>
2016-04-14 00:39:52 +0100

    Test: Refactor the footer partial view tests
    The footer partial view tests now make use
    of the the company details helper.

--- spec/views/shared/_footer.html.haml_spec.rb
+++ spec/views/shared/_footer.html.haml_spec.rb
@@ -1,9 +1,13 @@
 require "rails_helper"
 
 describe "shared/_footer.html.haml" do
+  let(:company_details) { CompanyDetailsHelper }
+
   it "displays the copyright" do
     render
-    expect(rendered).to match "Copyright &copy; #{Time.now.year} Methodic Limited"
+    expected_text = "Copyright &copy; #{Time.now.year} " +
+      company_details.registered_company[:name]
+    expect(rendered).to match expected_text
   end
 
   it "states 'All rights reserved'" do
@@ -13,16 +17,34 @@ describe "shared/_footer.html.haml" do
 
   it "indicates where the business is registered" do
     render
-    expect(rendered).to match "Methodic Limited is a company registered in England and Wales"
+    company_name     = company_details.registered_company[:name]
+    company_location = company_details.registered_company[:location]
+    expected_text    = "#{company_name} is a company registered in #{company_location}"
+    expect(rendered).to match expected_text
   end
 
   it "includes the company registration number" do
     render
-    expect(rendered).to match "9402598"
+    expect(rendered).to match company_details.registered_company[:number].to_s
   end
 
   it "includes the company VAT number" do
     render
-    expect(rendered).to match "205 7155 29"
+    expect(rendered).to match company_details.registered_company[:vat_registration]
+  end
+
+  it "includes the director's name" do
+    render
+    expect(rendered).to match company_details.director[:name]
+  end
+
+  it "includes the director's title" do
+    render
+    expect(rendered).to match company_details.director[:title]
+  end
+
+  it "includes the director's email" do
+    render
+    expect(rendered).to match company_details.director[:email]
   end
 end
