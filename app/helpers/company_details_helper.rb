c686977695daab2ca2d29cd673d3e38da5ee33ad
Edward Minnett <ed@methodic.io>
2016-04-14 00:38:24 +0100

    Feat: Add company details config Yaml

---
+++ app/helpers/company_details_helper.rb
@@ -0,0 +1,21 @@
+module CompanyDetailsHelper
+
+  def self.registered_company
+    config[:registered_company]
+  end
+
+  def self.director
+    config[:director]
+  end
+
+  private
+
+  def self.config_path
+    File.join(Rails.root, 'config', 'company_details.yml')
+  end
+
+  def self.config
+    YAML.load_file(config_path)
+  end
+
+end
