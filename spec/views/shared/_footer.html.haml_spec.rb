b7fd8366d272736404c2e7a02773cadaa0fed363
Edward Minnett <ed@methodic.io>
2016-04-14 00:05:08 +0100

    Test: Add tests for footer partial
    The website footer must include certain information.
    These tests ensure the information is present.
    
    Closes #41

--- spec/views/shared/_footer.html.haml_spec.rb
+++ spec/views/shared/_footer.html.haml_spec.rb
@@ -1,7 +1,28 @@
-require "spec_helper"
+require "rails_helper"
 
 describe "shared/_footer.html.haml" do
-  it "is true" do
-    expect(true).to be_truthy
+  it "displays the copyright" do
+    render
+    expect(rendered).to match "Copyright &copy; #{Time.now.year} Methodic Limited"
+  end
+
+  it "states 'All rights reserved'" do
+    render
+    expect(rendered).to match "All rights reserved"
+  end
+
+  it "indicates where the business is registered" do
+    render
+    expect(rendered).to match "Methodic Limited is a company registered in England and Wales"
+  end
+
+  it "includes the company registration number" do
+    render
+    expect(rendered).to match "9402598"
+  end
+
+  it "includes the company VAT number" do
+    render
+    expect(rendered).to match "205 7155 29"
   end
 end
