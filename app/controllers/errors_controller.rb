f9c25892a826f7c80418a37c39d75b6b1dbf0cae
Edward Minnett <ed@methodic.io>
2016-04-18 17:16:23 -0500

    Styling: Use colon style hash declarations
    Connects to #76

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -4,10 +4,10 @@
 # Controller for dynamic error pages.
 class ErrorsController < ApplicationController
   def not_found
-    render(:status => 404)
+    render(status: 404)
   end
 
   def internal_server_error
-    render(:status => 500)
+    render(status: 500)
   end
 end
